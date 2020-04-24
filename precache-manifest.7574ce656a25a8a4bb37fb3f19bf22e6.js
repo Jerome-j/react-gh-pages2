008c957526abb5f9b0ad58d3a5c7f85bf4f6ae94
Jerome-j <49815108+Jerome-j@users.noreply.github.com>
2020-04-25 03:34:51 +0800

    Updates

---
+++ precache-manifest.7574ce656a25a8a4bb37fb3f19bf22e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71eaf33aa972d8ac95bcc4d159cda653",
+    "url": "/react-gh-pages2/index.html"
+  },
+  {
+    "revision": "42a388c76a501448adbc",
+    "url": "/react-gh-pages2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "86c2c26a898d4fac6422",
+    "url": "/react-gh-pages2/static/js/2.80a30df2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages2/static/js/2.80a30df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a388c76a501448adbc",
+    "url": "/react-gh-pages2/static/js/main.150c500d.chunk.js"
+  },
+  {
+    "revision": "1667f3d22bc27b3da6d9",
+    "url": "/react-gh-pages2/static/js/runtime-main.313a5dca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages2/static/media/logo.5d5d9eef.svg"
+  }
+]);
